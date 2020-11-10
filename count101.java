a05a743c265c21cdc66cf796977da2f5ad20ee61
Yashaswee Kesharwani <69843696+yashasweeK9@users.noreply.github.com>
2020-11-10 15:24:35 +0530

    Update count101.java

--- count101.java
+++ count101.java
@@ -1,26 +1,25 @@
 import java.io.FileReader;
 import java.io.IOException;
-//`import java.io.BufferedReader;
 class count101 {
     public static void main(String[] args) throws IOException {
-        FileReader reader = new FileReader("input101.txt"); 
-        int expected[] = {49,48,49}; // A.S.C.I.I Values of the Searched String.
-        int size = expected.length; // Length of the String
-        int counter, tally; counter = tally = 0; // Counter : To count no. of Occurances in the File. Tally : To keep track of the progress of matching.
+        FileReader reader = new FileReader("input.txt"); 
+        int expected[] = {49,48,49}; // A.S.C.I.I Values of Each Character In The String to be Searched.
+        int size = expected.length; // Length of the String to be Searched.
+        int counter=0; // Counter : To count no. of Occurrences in the File.
+        int tally=0; // Tally : To Keep Track of Progress of Matching.    
         int i;
-        while((i=reader.read())!=-1) { // Traversing Through the File
+        while((i=reader.read())!=-1) { // Traversing Through A File, Character By Character
             if(i==expected[tally]) {
-                tally++; // Updating the Tracker
-
-                if(tally==size){
-                    counter++; // Updating the Occurance Count
-                    tally=1;
+                tally++; // Update the Tracker
+                if(tally==size) { // To Check the Tracker doesn't exceed the original length of the String To Be Searched.
+                    counter++; // Updating the Occurrence Counter
+                    tally=1; // As the Matched String conatins the Index 0 of the Expected String.
                 }
             }
             else
-                tally=0; 
+                tally=0; // Break the Match Streak 
         }
-        reader.close(); // Closing the File Reader
+        reader.close(); // Closing the reader Object
         System.out.println("No. of 101's in the Given File is: "+counter); //Print Statement
     }
 }
